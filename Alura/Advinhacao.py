ca5d123c5108ddaf3bd9aa94fa7903a29132b24e
Francisco Junior <franciscojdsjr@gmail.com>
2022-11-24 11:54:43 -0300

    modified:   Alura/Advinhacao.py

--- Alura/Advinhacao.py
+++ Alura/Advinhacao.py
@@ -1,10 +1,12 @@
+import random
 
 print('\n*****************************')
 print('Bem vindo ao jogo advinhação!')
 print('***************************** \n')
 
-numero_secreto = 13
+numero_secreto = round(random.random() * 100)
 total_de_tentativas = 5
+acertou = False
 
 
 ##while rodada <= total_de_tentativas: ## pode se usar While e o For
@@ -22,12 +24,16 @@ for rodada in range(1,total_de_tentativas + 1):
 
     if acertou:
         print('Você digitou {}, Você Acertou!!'.format(palpite))
+        acertou = True
         break
     else:
         if chute_maior:
             print('Você digitou {}, Você Errou!!, Seu chute foi maior que o número secreto.'.format(palpite))
         elif chute_menor:
             print('Você digitou {}, Você Errou!!, Seu chute foi menor que o número secreto.'.format(palpite))
+            
+if acertou == False:
+    print('\nVocê não conseguiu! o numero secreto era o {}\n'.format(numero_secreto))
             
 
 print('Fim do Jogo')
