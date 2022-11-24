c98e7a5317901d1b0c0723a0ddfa9dcacafed91e
Francisco Junior <franciscojdsjr@gmail.com>
2022-11-24 06:41:41 -0300

    On branch master  Changes to be committed: 	modified:   .gitignore 	new file:   Alura/Advinhacao.py 	new file:   Alura/Numpy/Python_Data_Science_Numpy.ipynb 	new file:   Alura/Numpy/data/carros-anos.txt 	new file:   Alura/Numpy/data/carros-km.txt 	new file:   Alura/Numpy/data/carros-nomes.txt 	new file:   Alura/Numpy/data/carros-valor.txt 	new file:   Alura/Numpy/data/carros-zero-km.txt

---
+++ Alura/Advinhacao.py
@@ -0,0 +1,34 @@
+
+print('\n*****************************')
+print('Bem vindo ao jogo advinhação!')
+print('***************************** \n')
+
+numero_secreto = 13
+total_de_tentativas = 5
+
+
+##while rodada <= total_de_tentativas: ## pode se usar While e o For
+for rodada in range(1,total_de_tentativas + 1):
+    print('Tentativa {} de {}'.format(rodada,(total_de_tentativas)))
+    palpite = int(input('Digite um número entre 1 e 100: '))
+    
+    if palpite < 1 or palpite > 100:
+        print('\nVocê deve digitar um numero entre 1 e 100\n')
+        continue
+    
+    acertou     = palpite == numero_secreto
+    chute_maior = palpite > numero_secreto
+    chute_menor = palpite < numero_secreto
+
+    if acertou:
+        print('Você digitou {}, Você Acertou!!'.format(palpite))
+        break
+    else:
+        if chute_maior:
+            print('Você digitou {}, Você Errou!!, Seu chute foi maior que o número secreto.'.format(palpite))
+        elif chute_menor:
+            print('Você digitou {}, Você Errou!!, Seu chute foi menor que o número secreto.'.format(palpite))
+            
+
+print('Fim do Jogo')
+    
