905a06d91ee34e6d01f02b553e1557a210a32cd3
francisco <franciscojdsjr@gmail.com>
2022-12-26 07:45:32 -0300

    modified:   Alura/Avancando_na_OO/__pycache__/filme.cpython-37.pyc 	modified:   Alura/Avancando_na_OO/filme.py

--- Alura/Avancando_na_OO/filme.py
+++ Alura/Avancando_na_OO/filme.py
@@ -1,23 +1,62 @@
 class Filme:
     
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
-        self.ano = ano
-        self.duracao = duracao
-        self.likes = 0
+        self.__nome = nome.title()
+        self.__ano = ano
+        self.__duracao = duracao
+        self.__likes = 0
+        
+    @property
+    def likes (self):
+        return self.__likes    
+
+    @property
+    def nome (self):
+        return self.__nome
+    
+    @property
+    def ano (self):
+        return self.__ano
+    
+    @property
+    def duracao (self):
+        return self.__duracao
 
     def dar_like (self):
-        self.likes += 1
+        self.__likes += 1
     
+    @nome.setter
+    def nome(self,novo_nome):
+        self.__nome = novo_nome.title()
     
         
 class Serie:
     
     def __init__(self, name, ano, temporadas):
-        self.name = name.title()
-        self.ano = ano
-        self.temporadas = temporadas
-        self.likes = 0
+        self.__name = name.title()
+        self.__ano = ano
+        self.__temporadas = temporadas
+        self.__likes = 0
+        
+    @property
+    def likes (self):
+        return self.__likes    
+
+    @property
+    def nome (self):
+        return self.__nome
+    
+    @property
+    def ano (self):
+        return self.__ano
+    
+    @property
+    def duracao (self):
+        return self.__duracao
 
     def dar_like (self):
-        self.likes += 1
+        self.__likes += 1
+    
+    @nome.setter
+    def nome(self,novo_nome):
+        self.nome = novo_nome.title()
