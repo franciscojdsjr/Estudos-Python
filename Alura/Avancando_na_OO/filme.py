b7611f6bde80b4b76def9c24433160858afc7495
francisco <franciscojdsjr@gmail.com>
2023-01-07 16:23:01 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   Alura/Avancando_na_OO/ABC.py
    	modified:   Alura/Avancando_na_OO/filme.py

--- Alura/Avancando_na_OO/filme.py
+++ Alura/Avancando_na_OO/filme.py
@@ -20,7 +20,7 @@ class Programa:
     def duracao (self):
         return self._duracao
 
-    def dar_likes (self):
+    def dar_like (self):
         self._likes += 1
     
     @nome.setter
@@ -48,18 +48,53 @@ class Serie(Programa):
     ##quando fazemos isso substituimos o metodo original
     def __str__ (self):
         return f'Nome: {self._nome}, Ano: {self._ano}, Temporadas {self._temporadas}, Likes: {self._likes}'
+
+class Playlist():
+    def __init__(self,nome,programas):
+        self.nome = nome
+        self._programas = programas
+        
+    def __getitem__(self,item):
+        return self._programas[item]
+        
+    @property
+    def listagem (self):
+        return self._programas
+
+    @property
+    def tamanho (self):
+        return len(self._programas)
+
+
+    
         
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+pantera_negra = Serie('Pantera Negra', 2016, 2)
 
-atlanta.dar_likes()
-atlanta.dar_likes()
-filmes_e_series = [vingadores, atlanta]
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()                                    
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
 
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'O tamanho da minha playlist é: {playlist_fim_de_semana.tamanho}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
  
+
+if pantera_negra in playlist_fim_de_semana:
+    print('Sim')
+else:
+    print('Não')
