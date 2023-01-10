9020330dbe12d276348bd5a84817198f2da2ae97
francisco <franciscojdsjr@gmail.com>
2023-01-10 06:28:12 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   Alura/Avancando_na_OO/ABC.py
    	modified:   Alura/Avancando_na_OO/alura.py
    	modified:   Alura/extrator-url/main.py

--- Alura/extrator-url/main.py
+++ Alura/extrator-url/main.py
@@ -1,2 +1,16 @@
 url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 print(url)
+
+indice_interrogação = url.find('?')
+
+print(indice_interrogação)
+
+url_base = url[:(indice_interrogação + 1)]
+print(url_base)
+
+url_parametros = url[(indice_interrogação + 1):]
+print(url_parametros)
+
+len('moedaorigem')
+
+print()
