686a64e07ff83a3be611d51462f21cb5b25af30b
francisco <franciscojdsjr@gmail.com>
2023-01-11 06:27:41 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   Alura/extrator-url/main.py

--- Alura/extrator-url/main.py
+++ Alura/extrator-url/main.py
@@ -1,16 +1,21 @@
 url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-print(url)
 
-indice_interrogação = url.find('?')
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
-print(indice_interrogação)
-
-url_base = url[:(indice_interrogação + 1)]
-print(url_base)
-
-url_parametros = url[(indice_interrogação + 1):]
+# Separa base e parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
-len('moedaorigem')
+# Busca o valor de um parâmetro
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 
-print()
+print(valor)
