1d6da46677cbae0a1ce4292d8e7416f592810d27
francisco <franciscojdsjr@gmail.com>
2023-01-13 06:52:25 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   Alura/extrator-url/extrator_url.py
    	modified:   Alura/extrator-url/main.py

--- Alura/extrator-url/main.py
+++ Alura/extrator-url/main.py
@@ -2,6 +2,14 @@ url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidad
 
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
+##sanitização da URL
+
+url = url.strip()
+
+##validação da URL
+if url == "":
+    raise ValueError('A sua URL está vazia')
+
 # Separa base e parâmetros
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
@@ -9,7 +17,7 @@ url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
 # Busca o valor de um parâmetro
-parametro_busca = 'moedaDestino'
+parametro_busca = 'quantidade'
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
 indice_e_comercial = url_parametros.find('&', indice_valor)
