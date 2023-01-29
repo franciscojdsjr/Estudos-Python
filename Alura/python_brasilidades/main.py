f1ca7197b781ad392a1ed51717e18e81d0d98c5e
francisco <franciscojdsjr@gmail.com>
2023-01-29 06:57:08 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   Alura/python_brasilidades/TelefonesBr.py
    	new file:   Alura/python_brasilidades/__pycache__/TelefonesBr.cpython-310.pyc
    	modified:   Alura/python_brasilidades/main.py

--- Alura/python_brasilidades/main.py
+++ Alura/python_brasilidades/main.py
@@ -20,12 +20,32 @@ padrao = "\w{5,50}@\w{3,10}.\w{2,3}.\w{2,3}"
 texto = "aaabbbcc rodrigo123@gmail.com.br"
 resposta = re.search(padrao, texto)
 
-print(resposta.group())
+#print(resposta.group())
 
 padrao1 = "\w{5,50}@\w{3,10}.\w{2,3}(.\w{2,3})?"
 texto1 = "aaabbccc franciscojdsjr@gmail.com.br"
 resposta1 = re.search(padrao1, texto1)
 
-print(resposta1.group())
+#print(resposta1.group())
 
 
+#####
+
+padrao_molde = '(xx) XXXXX-XXXX'
+
+padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+
+texto = 'eu gosto de pizza meu numero é 82991059072'
+
+resposta = re.findall(padrao,texto)
+
+#print(resposta)
+
+
+from TelefonesBr import TellefonesBr
+
+telefone = '558291059072'
+
+telefone_objeto = TellefonesBr(telefone)
+
+print(telefone_objeto)
