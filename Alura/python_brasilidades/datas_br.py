d35698a13f4f77d7d12e198c0c65a46b8bd6d8c6
francisco <franciscojdsjr@gmail.com>
2023-01-30 06:27:36 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   Alura/python_brasilidades/__pycache__/datas_br.cpython-310.pyc
    	new file:   Alura/python_brasilidades/datas_br.py
    	modified:   Alura/python_brasilidades/main.py
    	new file:   Alura/python_brasilidades/maintelefone.py

---
+++ Alura/python_brasilidades/datas_br.py
@@ -0,0 +1,31 @@
+from datetime import datetime,timedelta
+
+class DatasBr:
+    def __init__(self):
+        self.momento_cadastro = datetime.today()
+        
+    def mes_cadastro(self):
+        meses_do_ano = [
+            'janeiro','fevereiro','março',
+            'abril','maio','junho',
+            'julho,agosto,setembro',
+            'outubro','novembro','dezembro']
+        mes_cadastro = self.momento_cadastro.month - 1
+        return meses_do_ano[mes_cadastro]
+    
+    def dia_semana(self):
+        dias_semana = [
+            'segunda','terça','quarta','quinta',
+            'sexta','sabado','domingo'
+        ]
+        dia_semana = self.momento_cadastro.weekday() 
+        return dias_semana[dia_semana]
+    
+    def format(self):
+        formato_br = datetime.strftime(self.momento_cadastro,'%d/%m/%Y %H:%M:%S')
+        return formato_br
+        
+    def __str__(self) -> str:
+        return self.format()
+    
+    
