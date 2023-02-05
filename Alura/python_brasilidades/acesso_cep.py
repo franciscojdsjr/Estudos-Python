3b832fd54cad076c50b18a7e1989b28bfc723d33
francisco <franciscojdsjr@gmail.com>
2023-02-05 07:53:17 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   Alura/python_brasilidades/__pycache__/acesso_cep.cpython-310.pyc
    	new file:   Alura/python_brasilidades/acesso_cep.py
    	modified:   Alura/python_brasilidades/main.py

---
+++ Alura/python_brasilidades/acesso_cep.py
@@ -0,0 +1,23 @@
+class BuscaEndereco:
+    
+    def __init__(self,cep):
+        cep = str(cep)
+        if self.cep_e_valido(cep):
+            self.cep = cep
+        else:
+            raise ValueError('CEP Invalido')
+        
+    
+    def cep_e_valido(self,cep):
+        if len(cep) == 8:
+            return True
+        else:
+            return False
+    
+    def __str__(self):
+        return self.format_cep()
+    
+    def format_cep(self):
+        return "{}-{}".format(self.cep[:5],self.cep[5:])
+    
+    
--- Alura/python_brasilidades/main.py
+++ Alura/python_brasilidades/main.py
@@ -1,11 +1,16 @@
 from datetime import datetime, timedelta
 from datas_br import DatasBr
+from acesso_cep import BuscaEndereco
 
 ##print(datetime.today())
 
 
-datas = DatasBr()
-
-print(datas.tempo_cadastro())
+##datas = DatasBr()
+##
+##print(datas.tempo_cadastro())
 
 #asims
+
+cep = 57081760
+objeto_cep = BuscaEndereco(cep)
+print(objeto_cep)
