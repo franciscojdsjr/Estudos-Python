fbde1828fc4fc90b273fc4ada0a601a264d62147
francisco <franciscojdsjr@gmail.com>
2023-02-06 07:04:25 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   Alura/testes_unitarios/__pycache__/codigo.cpython-310.pyc
    	modified:   Alura/testes_unitarios/main.py

--- Alura/testes_unitarios/main.py
+++ Alura/testes_unitarios/main.py
@@ -2,10 +2,16 @@ from codigo import Funcionarios
 from datetime import datetime
 
 
-func = Funcionarios('Francisco','29/12/1993','5000')
-
+#func = Funcionarios('Francisco','29/12/1993','5000')
 ##print(func.idade())
-
-print(func.idade())
-
+#print(func.idade())
 ##
+
+
+def teste_idade():
+    funciorio_teste = Funcionarios('Teste','06/02/2000','1158')
+    print('Teste = {}'.format(funciorio_teste.idade()))
+    
+
+teste_idade()
+    
