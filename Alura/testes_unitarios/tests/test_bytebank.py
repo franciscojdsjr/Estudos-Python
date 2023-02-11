94f162eb76290364cf835ee4266db0ad48c52fad
francisco <franciscojdsjr@gmail.com>
2023-02-11 08:19:04 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   Alura/testes_unitarios/pytest.ini
    	modified:   Alura/testes_unitarios/tests/__pycache__/test_bytebank.cpython-37-pytest-7.1.2.pyc
    	modified:   Alura/testes_unitarios/tests/test_bytebank.py

--- Alura/testes_unitarios/tests/test_bytebank.py
+++ Alura/testes_unitarios/tests/test_bytebank.py
@@ -1,5 +1,6 @@
 from bytebank.codigo import Funcionarios
 import pytest
+from pytest import mark
 
 class TestClass:
     
@@ -31,6 +32,7 @@ class TestClass:
         
         assert resultado == esperado #then 
     
+    @mark.calcular_bonus
     def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
         entrada = 1000 #given
         esperado = 100
@@ -40,6 +42,7 @@ class TestClass:
 
         assert resultado == esperado #then
     
+    @mark.calcular_bonus 
     def test_quando_calcular_bonus_recebe_1000000_deve_retornar_exception(self):
         with pytest.raises(Exception):
             entrada = 1000000 #given
