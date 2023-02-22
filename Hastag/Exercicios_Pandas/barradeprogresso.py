749ad6ded4614379298d77ab993e640e3ae69eec
francisco <franciscojdsjr@gmail.com>
2023-02-22 05:59:56 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   "Hastag/Exercicios_Pandas/18.13 Exerc\303\255cio Pandas.ipynb"
    	modified:   "Hastag/Exercicios_Pandas/18.13 Gabarito - Exerc\303\255cio 5 (1).ipynb"
    	modified:   Hastag/Exercicios_Pandas/Gabarito - Pandas 04.ipynb
    	new file:   Hastag/Exercicios_Pandas/__pycache__/tqdm.cpython-37.pyc
    	renamed:    Hastag/Exercicios_Pandas/tqdm.py -> Hastag/Exercicios_Pandas/barradeprogresso.py

--- Hastag/Exercicios_Pandas/barradeprogresso.py
+++ Hastag/Exercicios_Pandas/barradeprogresso.py
@@ -1,10 +1,12 @@
-
+from tqdm import tqdm
 import pandas as pd
+
+
 #importando os arquivos
-vendas_df = pd.read_csv(r'Contoso - Vendas  - 2017.csv', sep=';')
-produtos_df = pd.read_csv(r'Contoso - Cadastro Produtos.csv', sep=';')
-lojas_df = pd.read_csv(r'Contoso - Lojas.csv', sep=';')
-clientes_df = pd.read_csv(r'Contoso - Clientes.csv', sep=';')
+vendas_df = pd.read_csv(r'/home/francisco/Devlopment/Python Scripts/Hastag/Exercicios_Pandas/Contoso - Vendas  - 2017.csv', sep=';')
+produtos_df = pd.read_csv(r'/home/francisco/Devlopment/Python Scripts/Hastag/Exercicios_Pandas/Contoso - Cadastro Produtos.csv', sep=';')
+lojas_df = pd.read_csv(r'/home/francisco/Devlopment/Python Scripts/Hastag/Exercicios_Pandas/Contoso - Lojas.csv', sep=';')
+clientes_df = pd.read_csv(r'/home/francisco/Devlopment/Python Scripts/Hastag/Exercicios_Pandas/Contoso - Clientes.csv', sep=';')
 
 #limpando apenas as colunas que queremos
 clientes_df = clientes_df[['ID Cliente', 'E-mail']]
@@ -15,7 +17,6 @@ lojas_df = lojas_df[['ID Loja', 'Nome da Loja']]
 vendas_df = vendas_df.merge(produtos_df, on='ID Produto')
 vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
 vendas_df = vendas_df.merge(clientes_df, on='ID Cliente').rename(columns={'E-mail': 'E-mail do Cliente'})
-display(vendas_df)
 
 
 
@@ -25,6 +26,8 @@ for i, id_loja in enumerate(vendas_df['ID Loja']):
     if id_loja == 222:
         vendas_df.loc[i, 'Quantidade Devolvida'] += 1
     pbar.update()
+
+
 
 
     
